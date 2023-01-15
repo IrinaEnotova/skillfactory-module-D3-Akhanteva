df90067ede41e253be1ef5e7ea927bb755ebfa69
IrinaEnotova <102694649+IrinaEnotova@users.noreply.github.com>
2023-01-15 15:35:24 +0400

    feat: add solution for D3

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import ListView, DetailView
+from .models import *
+
+class NewsList(ListView):
+  model = Post
+  template_name = 'news.html'
+  context_object_name = 'news'
+
+class NewsDetail(DetailView):
+  model = Post
+  template_name = 'post.html'
+  context_object_name = 'post'
